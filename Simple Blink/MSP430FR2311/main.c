04f6f95f188c60fc22dcc7dd8c53ff69207bfc99
chipable <pager6@students.rowan.edu>
2017-09-12 19:41:18 -0400

    added working code

--- Simple Blink/MSP430FR2311/main.c
+++ Simple Blink/MSP430FR2311/main.c
@@ -1,13 +1,27 @@
-#include <msp430.h> 
-
-
-/**
+/* Lab 2 Simple Blink
+ * MSP430FR2311
+ * 9/11/2017
+ * Robert Page
  * main.c
  */
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
 
-	return 0;
+#include <msp430.h> 
+
+int main(void){
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	PM5CTL0 &= ~LOCKLPM5;  // adjust power settings
+
+	/* GPIO INIT*/
+	P1SEL0 &= ~(1<<0);       // sets port mux
+	P1SEL1 &= ~(1<<0);       // sets port mux
+	P1DIR |= (1<<0);    // Sets P1.0 as an output
+
+	    /* LED FLASHING*/
+        while(1){
+            _delay_cycles(500000);   //waits 500,000 clk cycles
+            P1OUT |=(1<<0); // Sets P1.0 High
+
+            _delay_cycles(500000);   //waits 500,000 clk cycles
+            P1OUT &= ~(1<<0);   // Sets P1.0 low
+        }
 }
