350bdbb98b2b1460e975e1e46f1033a369598e0b
chipable <pager6@students.rowan.edu>
2017-09-12 22:09:13 -0400

    added working code without a debounce

---
+++ Button Blink/MSP430FR2311/main.c
@@ -0,0 +1,34 @@
+/* Lab 2 Button LED Toggle
+ * MSP430FR2311
+ * 9/12/2017
+ * Robert Page
+ * main.c
+ */
+
+#include <msp430.h> 
+
+int main(void){
+
+int state = 1;
+
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;  // adjust power settings
+
+    /* GPIO INIT*/
+    P1SEL0 &= ~(1<<0);       // sets port mux
+    P1SEL1 &= ~(1<<0);       // sets port mux
+    P1DIR |= (1<<0);    // Sets P1.0 as an output
+    P1DIR &= ~(1<<1);   // sETS P1.1 as an input
+    P1REN |= (1<<1);    // Enables pull up resistor on P1.1
+    P1OUT |= (1<<1);    // Sets pull up to VCC
+
+        /* Button Enabled LED*/
+        while(1){
+            if((~P1IN & (1<<1))&&state){
+                P1OUT ^=(1<<0); // Toggles P1.0
+                state = 0;
+            }
+            else if(P1IN & (1<<1))
+                state = 1;
+        }
+}
