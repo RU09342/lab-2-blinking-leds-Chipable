e6b9ace0e72c4fc67bd0f3ffd8e8687b99ccbe7f
chipable <pager6@students.rowan.edu>
2017-09-18 14:08:26 -0400

    added comments

--- Button Blink/MSP430F5529/main.c
+++ Button Blink/MSP430F5529/main.c
@@ -14,19 +14,19 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
     /* INIT GPIO*/
-    P1DIR |= BIT0;
-    P1DIR &= ~BIT1;
-    P1SEL &= ~(BIT0|BIT1);
-    P1REN |= BIT1;
-    P1OUT |= BIT1;
+    P1DIR |= BIT0;              // sets P1.0 to output
+    P1DIR &= ~BIT1;             // sets P1.1 to input
+    P1SEL &= ~(BIT0|BIT1);      // sets P1.0 and P1.1 mux
+    P1REN |= BIT1;              // Enables P1.1 resistor
+    P1OUT |= BIT1;              // configures P1.1 resistor as pull-up
 	
 	/* Button Enabled LED*/
     while(1){
-        if((~P1IN & BIT1)&&state){
-            P1OUT ^= BIT0; // Toggles P1.0
-            state = 0;
+        if((~P1IN & BIT1)&&state){          //enters on button press and state == 1
+            P1OUT ^= BIT0;                  // Toggles P1.0
+            state = 0;                      // prevents re-entry before button is released
         }
-        else if(P1IN & (1<<1))
-            state = 1;
+        else if(P1IN & (1<<1))              // enters on button released
+            state = 1;                      // resets state
     }
 }
